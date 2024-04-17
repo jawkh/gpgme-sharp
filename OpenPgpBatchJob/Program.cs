31d4c47bafa3cd1767c21c7d1ce19782ee49a337
jawkh <jonathan.aw.k.h@gmail.com>
2024-04-17 21:36:57 +0800

    Updated OpenPGPBatchjob to print out the details of Sender and Recipient Keys
    Detailed properties of the Sender's and Recipient's OpenPGP keys will be printed into the logs at the start of the job run. This information aids in troubleshooting.

--- OpenPgpBatchJob/Program.cs
+++ OpenPgpBatchJob/Program.cs
@@ -10,6 +10,7 @@ using System.Security.Cryptography.Xml;
 using System.Linq;
 using OpenPgpBatchJob;
 using Serilog;
+using System.Reflection;
 //using Microsoft.Extensions.Logging;
 
 namespace OpenPgpBatchJob
@@ -83,6 +84,9 @@ namespace OpenPgpBatchJob
 
             sb.Append("Ok!");
             Log.Information(sb.ToString());
+
+            Log.Information(string.Format("Sender's {0}", PrintPropertiesOfOpenPGPKey(openPgpHelper.SenderKey)));
+            Log.Information(string.Format("Recipient's {0}", PrintPropertiesOfOpenPGPKey(openPgpHelper.RecipientKey)));
 
             sb.Clear();
             sb.Append("Verifying Source and Destination Folder Paths...");
@@ -199,8 +203,79 @@ namespace OpenPgpBatchJob
         }
 
 
+        /// <summary>
+        /// Print the Properties of an PgpKey object and the properties of all its subkeys
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static string PrintPropertiesOfOpenPGPKey(Libgpgme.PgpKey key)
+        {
+            // Printing the Key Properties
+            var keyPropertiesToPrint = new List<string> { "Uid", "KeyId", "Fingerprint", "Protocol", "Revoked", "Expired", "Disabled", "Invalid", "IsQualified", "OwnerTrust", "KeylistMode", "CanAuthenticate", "CanCertify", "CanEncrypt", "CanSign", "Secret" };
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Key Properties: ");
+            sb.AppendLine(PrintProperties(key, keyPropertiesToPrint).ToString());
 
+            // Printing the SubKeys properties
+            var subKeyPropertiesToPrint = new List<string> { "KeyId", "Fingerprint", "Timestamp", "TimestampUTC", "Revoked", "Expired", "Expires", "ExpiresUTC", "Disabled", "Invalid", "IsQualified", "IsInfinitely", "CanAuthenticate", "CanCertify", "CanEncrypt", "CanSign", "Curve", "Secret", "Length", "PubkeyAlgorithm" };
+            sb.AppendLine("Subkey 1 Properties: ");
+            sb.AppendLine(PrintProperties(key.Subkeys, subKeyPropertiesToPrint).ToString());
+
+            if (key.Subkeys.Next != null)
+            {
+                sb.AppendLine("Subkey 2 Properties: ");
+                sb.AppendLine(PrintProperties(key.Subkeys, subKeyPropertiesToPrint).ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// This function print out the list of specified properties of an Object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="propertyNames"></param>
+        private static StringBuilder PrintProperties(object obj, List<string> propertyNames)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (obj == null)
+            {
+                Console.WriteLine("Object is null.");
+                return null;
+            }
+
+            Type type = obj.GetType();
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (string propName in propertyNames)
+            {
+                PropertyInfo property = type.GetProperty(propName);
+
+                if (property != null)
+                {
+                    try
+                    {
+                        object value = property.GetValue(obj, null);
+                        sb.AppendLine($"{property.Name} = {value}");
+                    }
+                    catch (Exception ex)
+                    {
+                        sb.AppendLine($"{property.Name} could not be read: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    sb.AppendLine($"Property '{propName}' does not exist on {type.Name}.");
+                }
+            }
+
+            return sb;
+        }
     }
 
 
 }
+
+
+
